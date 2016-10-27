1781483274b8c45cdf15a21e9bc8d0d1e12d16c8
Alex Chen <alexanderchenmh@gmail.com>
2016-10-28 00:46:01 +0800

    update

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,17 @@
-'use strict';
+const zoom = () => {
+  chrome.storage.local.get(
+    null,
+    ({zoom, autoResize}) => {
+      if (!autoResize) {
+        return
+      }
+      document.body.parentElement.style.zoom = `${zoom}%`
+    }
+  )
+}
 
-console.log('\'Allo \'Allo! Content script');
+chrome.storage.local.get('autoResize', ({autoResize}) => {
+  if (autoResize) {
+    zoom()
+  }
+})
--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -7,30 +7,77 @@ class App extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state = {autoResize: false}
+    this.state = {autoResize: false, zoom: 100}
     const self = this
-    chrome.storage.local.get('autoResize', ({autoResize}) => {
-      self.setState({autoResize})
-      chrome.browserAction.setBadgeText({text: autoResize? 'ON': ''});
+    chrome.storage.local.get(null, ({zoom, autoResize}) => {
+      self.setState({autoResize, zoom})
+      chrome.browserAction.setBadgeText({text: autoResize ? 'ON' : ''});
     })
   }
 
   switchMe() {
-    const autoResize = !this.state.autoResize
+    let {autoResize, zoom} = this.state
+    autoResize = !autoResize
     const self = this
-    chrome.storage.local.set({autoResize}, () => {
-      self.setState({autoResize})
-      chrome.browserAction.setBadgeText({text: autoResize? 'ON': ''});
+    chrome.storage.local.set({zoom, autoResize}, () => {
+      self.setState({zoom, autoResize})
+      chrome.browserAction.setBadgeText({text: autoResize ? 'ON' : ''});
     })
   }
 
+  resetZoom() {
+    let {zoom, autoResize} = this.state
+    zoom = 100
+    const self = this
+    chrome.storage.local.set({zoom, autoResize}, () => {
+      self.setState({zoom, autoResize})
+    })
+  }
+
+
+  incrZoom() {
+    let {zoom, autoResize} = this.state
+    zoom += 10
+    const self = this
+    chrome.storage.local.set({zoom, autoResize}, () => {
+      self.setState({zoom, autoResize})
+    })
+  }
+
+  decrZoom() {
+    let {zoom, autoResize} = this.state
+    zoom -= 10
+    const self = this
+    chrome.storage.local.set({zoom, autoResize}, () => {
+      self.setState({zoom, autoResize})
+    })
+  }
+
+
   render() {
-    const auto = this.state.autoResize
-    const style = {color: auto? '#00FF00':'gray', fontSize: '3em'}
+    const {autoResize, zoom} = this.state
+    if (autoResize) {
+      chrome.tabs.executeScript({code: `document.body.parentElement.style.zoom = '${zoom}%'`})
+    } else {
+      chrome.tabs.executeScript({code: `document.body.parentElement.style.zoom = '100%'`})
+    }
+
     return (
-      <h1 style={style} onClick={() => this.switchMe()}>
-        {auto? '🔴':'⚪'}
-      </h1>
+      <div>
+        <div className="switch">
+          <a className={autoResize ? 'on' : 'off'} onClick={() => this.switchMe()}>
+            {autoResize ? 'No' : 'Off'}
+          </a>
+        </div>
+        <h1>{zoom}%</h1>
+        <div className="zoom">
+          <button onClick={() => this.resetZoom()}>=</button>
+          &nbsp;
+          <button onClick={() => this.decrZoom()}>-</button>
+          &nbsp;
+          <button onClick={() => this.incrZoom()}>+</button>
+        </div>
+      </div>
     )
   }
 }
